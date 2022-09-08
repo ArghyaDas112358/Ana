aaf9f2da99486b00a88b04ac1957f7b26034c48e
Marc Huwiler <marc.huwiler@cern.ch>
2022-09-08 19:19:39 +0200

    Adding loading in wrapper

--- ApplyTFweight.sh
+++ ApplyTFweight.sh
@@ -1,1 +1,2 @@
-root -e 'TPython::LoadMacro("TFEvaluation.py");' ApplyTFweight.C
+#root -e 'gInterpreter->AddIncludePath("/opt/local/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages/numpy/core/include");' TestApplyTFweightnew.C
+root -e 'gInterpreter->AddIncludePath("/opt/local/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages/numpy/core/include");' ApplyTFweight.C

50e6193f5ca55c6eed26716a49ab99c011e99d3f
Marc Huwiler <marc.huwiler@cern.ch>
2022-09-08 14:17:28 +0200

    Changing to new file with multiple taus

--- TestApplyTFweight.C
+++ TestApplyTFweight.C
@@ -210,6 +210,8 @@ void TestApplyTFweight(TString campaignName = "ApplyTFweight/")
 	filemanager.AddItem("MCSignal", "/eos/home-m/mhuwiler/DoctoralThesis/Analysis/data/PrivateProductionGenDstar_converted.root", "tree"); 
 	filemanager.AddItem("SignalOfficialMC50M", "/eos/home-m/mhuwiler/DoctoralThesis/Analysis/data/SignalOfficialMC50M.root", "ntuplizer/tree"); 
 	filemanager.AddItem("ParkingBPHAllRun2018B", "/eos/home-m/mhuwiler/DoctoralThesis/Analysis/data/ParkingBPHRun2018B_converted.root", "tree"); 
+    filemanager.AddItem("MCSignalMMultipleTau", "/eos/home-m/mhuwiler/DoctoralThesis/Analysis/data/firstAllTau.root", "ntuplizer/tree"); 
+
 
 
 	filemanager.OpenAllItems(); 
@@ -217,7 +219,7 @@ void TestApplyTFweight(TString campaignName = "ApplyTFweight/")
 	gStyle->SetOptStat(0); 
 
 
-	auto dataframe = RDataFrame(*filemanager.GetItem<TTree*>("SignalOfficialMC50M")); // tree100k
+	auto dataframe = RDataFrame(*filemanager.GetItem<TTree*>("MCSignalMMultipleTau")); // tree100k
 	
 
 	// Defining the delta

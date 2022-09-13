38a69b9a298c290d0cab3d24db6d7ad6d55281cd
Marc Huwiler <marc.huwiler@cern.ch>
2022-09-13 17:21:47 +0200

    Enable multithreading

--- ApplyTFweight.C
+++ ApplyTFweight.C
@@ -134,7 +134,7 @@ std::vector<double> castVector(std::vector<float> vec)
 
 void ApplyTFweight(TString campaignName = "ApplyTFweight/") 
 {
-	ROOT::DisableImplicitMT(); 
+	ROOT::EnableImplicitMT(); //ROOT::DisableImplicitMT(); 
 	//gROOT->LoadMacro("/Users/mhuwiler/coding/plugins/FileManager/CFileManager.C"); 
 	//gROOT->LoadMacro("/Users/mhuwiler/coding/plugins/FileManager/CFileManager.C+");
 	//gSystem->Load("/Users/mhuwiler/coding/plugins/FileManager/CFileManager.so"); 
@@ -329,7 +329,7 @@ void ApplyTFweight(TString campaignName = "ApplyTFweight/")
 		return response; 
 	};
 
-	auto withWeight = dataframe.Range(0, 10).Define("TFscore", TFresponse, {"BsDstarTauNu_Ds_pt", "BsDstarTauNu_Ds_eta", "BsDstarTauNu_Ds_phi", "BsDstarTauNu_spi_charge", "track_pt", "track_eta", "track_phi", "track_charge", "track_doca2D", "track_doca2Derror", "track_doca", "track_docaerror", "track_dzToPV", "track_dzToClosestVertex", "track_isAssociatedToPV", "track_pvAssociationQuality", "track_isgenmatched"}); 
+	auto withWeight = dataframe.Define("TFscore", TFresponse, {"BsDstarTauNu_Ds_pt", "BsDstarTauNu_Ds_eta", "BsDstarTauNu_Ds_phi", "BsDstarTauNu_spi_charge", "track_pt", "track_eta", "track_phi", "track_charge", "track_doca2D", "track_doca2Derror", "track_doca", "track_docaerror", "track_dzToPV", "track_dzToClosestVertex", "track_isAssociatedToPV", "track_pvAssociationQuality", "track_isgenmatched"}); 
 
 	withWeight.Snapshot("ntuplizer/tree", "SignalOfficialMC50M_withTHweight.root"); 
 

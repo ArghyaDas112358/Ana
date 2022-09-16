0a1c799b87d56795b3fe0662571ea5175c081d0f
Marc Huwiler <marc.huwiler@cern.ch>
2022-09-16 23:51:23 +0200

    First version with tau class

---
+++ Tau.h
@@ -0,0 +1,37 @@
+#ifndef TAU_H
+#define TAU_H
+#ifdef __MAKECINT__ 
+#pragma link C++ class vector<Tau>; 
+#endif // __MAKECINT__
+
+#include <TObject.h>
+#include <vector>
+#include <map>
+
+
+class Tau : public TObject 
+{
+  public: 
+	float pt; 
+	float eta; 
+	float phi; 
+	int q; 
+	float m; 
+	int idx1;
+	int idx2; 
+	int idx3; 
+	float sumdnn; 
+	float dnn1; 
+	float dnn2; 
+	float dnn3; 
+
+	ClassDef(Tau, 0);
+
+}; 
+
+
+typedef std::vector<Tau> TauCollection; 
+//typedef std::map<Tau, float> PtOrderedTauCollection; 
+
+
+#endif 

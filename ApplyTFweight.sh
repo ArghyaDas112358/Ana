c54dae971d467db0117a30595c370ef6e6c55c4b
Marc Huwiler <marc.huwiler@cern.ch>
2022-10-25 14:06:45 +0200

    Pass ref as arguments to wrapper script

--- ApplyTFweight.sh
+++ ApplyTFweight.sh
@@ -8,7 +8,25 @@ else
 	echo "Unknown environment "$ENVDEF
 fi
 
+if [[ $# == 0 ]]; then
+	echo "ERROR: No argument provided. You need to at least provide the ref of the bare file."
+	return 5
+elif [[ $# > 2 ]]; then
+	echo "WARNING: Too many arguments provided. Arguments after 2nd position will be ignored. "
+fi 
+
+INITIALREF=${1}
+FINALREF="${INITIALREF}_DNN_test" # Adding suffix to the ref
+#Remove suffix from string
+#FINALREF=${INITIALREF%".root"}
+if [[ $# == 2 ]]; then
+	FINALREF=${2}
+fi
+
+echo $INITIALREF
+echo $FINALREF
+
 #root -e 'gInterpreter->AddIncludePath("/opt/local/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages/numpy/core/include");' TestApplyTFweightnew.C
-root -e 'gInterpreter->AddIncludePath("'$NUMPYLIBRARY'");' 'ApplyTFweight.C("SignalOfficialMC50M", "SignalOfficialMC50M_test")'
+root -e 'gInterpreter->AddIncludePath("'$NUMPYLIBRARY'");' 'ApplyTFweight.C("'$INITIALREF'", "'$FINALREF'")'
 #root -e 'gInterpreter->AddIncludePath("/opt/local/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages/numpy/core/include");' ApplyTFweight.C
 #root -e 'gInterpreter->AddIncludePath("/t3home/mhuwiler/.local/lib/python3.6/site-packages/numpy/core/include/");' ApplyTFweight.C

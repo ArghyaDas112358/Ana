1114974735230d0c12310a70124da89034900473
Marc Huwiler <marc.huwiler@cern.ch>
2022-10-25 14:16:08 +0200

    Added ability to pass max number of events

--- ApplyTFweight.C
+++ ApplyTFweight.C
@@ -133,7 +133,7 @@ std::vector<double> castVector(std::vector<float> vec)
 
  
 
-void ApplyTFweight(const TString& inIdentifier, const TString& outIndentifier) 
+void ApplyTFweight(const TString& inIdentifier, const TString& outIndentifier, const Int_t Nmax = 0) 
 {
 	//ROOT::EnableImplicitMT(); //ROOT::DisableImplicitMT(); 
 	//gROOT->LoadMacro("/Users/mhuwiler/coding/plugins/FileManager/CFileManager.C"); 
@@ -302,7 +302,7 @@ void ApplyTFweight(const TString& inIdentifier, const TString& outIndentifier)
 		return response; 
 	};
 
-	auto withWeight = dataframe.Range(0, 100).Define("TFscore", TFresponse, {"BsDstarTauNu_Ds_pt", "BsDstarTauNu_Ds_eta", "BsDstarTauNu_Ds_phi", "BsDstarTauNu_spi_charge", "track_pt", "track_eta", "track_phi", "track_charge", "track_doca2D", "track_doca2Derror", "track_doca", "track_docaerror", "track_dzToPV", "track_dzToClosestVertex", "track_isAssociatedToPV", "track_pvAssociationQuality", "track_isgenmatched"}); 
+	auto withWeight = dataframe.Range(0, Nmax).Define("TFscore", TFresponse, {"BsDstarTauNu_Ds_pt", "BsDstarTauNu_Ds_eta", "BsDstarTauNu_Ds_phi", "BsDstarTauNu_spi_charge", "track_pt", "track_eta", "track_phi", "track_charge", "track_doca2D", "track_doca2Derror", "track_doca", "track_docaerror", "track_dzToPV", "track_dzToClosestVertex", "track_isAssociatedToPV", "track_pvAssociationQuality", "track_isgenmatched"}); 
 
 	withWeight.Snapshot(filemanager.GetObject(outIndentifier), filemanager.GetFile(outIndentifier)); 
 
--- ApplyTFweight.sh
+++ ApplyTFweight.sh
@@ -19,6 +19,7 @@ INITIALREF=${1}
 FINALREF="${INITIALREF}_DNN_test" # Adding suffix to the ref
 #Remove suffix from string
 #FINALREF=${INITIALREF%".root"}
+MAXEVENTS=10
 if [[ $# == 2 ]]; then
 	FINALREF=${2}
 fi
@@ -27,6 +28,6 @@ echo $INITIALREF
 echo $FINALREF
 
 #root -e 'gInterpreter->AddIncludePath("/opt/local/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages/numpy/core/include");' TestApplyTFweightnew.C
-root -e 'gInterpreter->AddIncludePath("'$NUMPYLIBRARY'");' 'ApplyTFweight.C("'$INITIALREF'", "'$FINALREF'")'
+root -e 'gInterpreter->AddIncludePath("'$NUMPYLIBRARY'");' 'ApplyTFweight.C("'$INITIALREF'", "'$FINALREF'", '$MAXEVENTS')'
 #root -e 'gInterpreter->AddIncludePath("/opt/local/Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages/numpy/core/include");' ApplyTFweight.C
 #root -e 'gInterpreter->AddIncludePath("/t3home/mhuwiler/.local/lib/python3.6/site-packages/numpy/core/include/");' ApplyTFweight.C

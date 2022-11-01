dc2fc75201f07a4a0e71f8a1a19e9ee5822b9dad
Marc Huwiler <marc.huwiler@cern.ch>
2022-11-01 23:24:30 +0100

    Edited the fom plot for October 2022 results

--- PlotRoc.py
+++ PlotRoc.py
@@ -249,7 +249,7 @@ if __name__ == "__main__":
 	print sigeffcorr 
 	print bkgeffcorr
 
-	fom = GetFom(sigeffcorr, bkgeffcorr)
+	fom = GetFom(sigeffcorr, bkgeffcorr, 626, 2698) #626, 2698 updated values from Oct. 2022
 
 	# Margins 
 	leftmargin = 0.1
@@ -257,14 +257,15 @@ if __name__ == "__main__":
 	bottommargin = 0.15
 	topmargin = 0.1
 
-	cut1 = 0.7
-	cut2 = -0.2
+	cut1 = 0.9
+	cut2 = 0.0
+	cut3 = -0.5
 
 	ROOT.gStyle.SetPadTickY(0) # Disactivate axes on both sides
 
 	fomcanvas = ROOT.TCanvas("fomcanvas", "fomcanvas", 800, 600)
 	fomcanvas.SetMargin(leftmargin, rightmargin, bottommargin, topmargin); 
-	signalDist = ROOT.RDataFrame(filemanager.GetItem("signal")).Filter("mvaScore>-1").Histo1D(("signalDist", "signalDist", 100, -1., 1.), "mvaScore")
+	signalDist = ROOT.RDataFrame(filemanager.GetItem("signal")).Filter("mvaScore>-1").Histo1D(("signalDist", "signalDist", 100, -1., 1.), "mvaScore") # TODO: use cut >= -1.
 	bkgDist = ROOT.RDataFrame(filemanager.GetItem("background")).Filter("mvaScore>-1").Histo1D(("bkgDist", "bkgDist", 100, -1., 1.), "mvaScore")
 	fomcanvas.cd()
 	fompad = ROOT.TPad("fompad", "fompad", 0., 0., 1., 1.)
@@ -300,6 +301,7 @@ if __name__ == "__main__":
 	fomcanvas.cd()
 	pad.SetLogy()
 	pad.Draw()
+	fom.SetMarkerSize(0.2)
 	fom.SetLineColor(ROOT.kGreen+2)
 	#fom.SetMarkerColor(ROOT.kGreen+3)
 	#fom.SetMarkerColor(ROOT.kGreen)
@@ -307,7 +309,7 @@ if __name__ == "__main__":
 	#fom.SetMarkerSize(2)
 	fomcanvas.Draw()
 
-	fomlegend = ROOT.TLegend(0.15, 0.7, 0.35, 0.85)
+	fomlegend = ROOT.TLegend(0.125, 0.7, 0.305, 0.85)
 	fomlegend.AddEntry(fom, "significance")
 	fomlegend.AddEntry(signalDist.GetPtr(), "signal")
 	fomlegend.AddEntry(bkgDist.GetPtr(), "background")
@@ -320,6 +322,7 @@ if __name__ == "__main__":
 	factor = (1-leftmargin-rightmargin)
 	value1 = leftmargin + (cut1 - fom.GetXaxis().GetXmin())/(fom.GetXaxis().GetXmax() - fom.GetXaxis().GetXmin())*(1-leftmargin-rightmargin)
 	value2 = leftmargin + (cut2 - fom.GetXaxis().GetXmin())/(fom.GetXaxis().GetXmax() - fom.GetXaxis().GetXmin())*(1-leftmargin-rightmargin)
+	value3 = leftmargin + (cut3 - fom.GetXaxis().GetXmin())/(fom.GetXaxis().GetXmax() - fom.GetXaxis().GetXmin())*(1-leftmargin-rightmargin)
 	print fom.GetXaxis().GetXmax()
 	print fom.GetXaxis().GetXmin()
 	print frac
@@ -327,12 +330,20 @@ if __name__ == "__main__":
 	print frac*factor
 	print value1
 	line1 = ROOT.TLine(value1, bottommargin, value1, 1-topmargin)
+	#line1.SetLineStyle(7)
+	line1.SetLineColor(ROOT.kGray+2)
 	line1.Draw()
 
 	line2 = ROOT.TLine(value2, bottommargin, value2, 1-topmargin)
+	line2.SetLineColor(ROOT.kGray+2)
 	line2.Draw()
 
-	label1 = ROOT.TPaveText(0.22, 0.5, 0.3, 0.6)
+	line3 = ROOT.TLine(value3, bottommargin, value3, 1-topmargin)
+	line3.SetLineColor(ROOT.kGray+2)
+	#line3.SetLineStyle(7)
+	line3.Draw()
+
+	label1 = ROOT.TPaveText(0.35, 0.6, 0.45, 0.7)
 	label1.AddText("modelling")
 	label1.AddText("region")
 	label1.SetBorderSize(0)
@@ -342,7 +353,7 @@ if __name__ == "__main__":
   	label1.SetTextColor(ROOT.kGray+2)
 	label1.Draw()
 
-	label2 = ROOT.TPaveText(0.45, 0.5, 0.6, 0.6)
+	label2 = ROOT.TPaveText(0.6, 0.5, 0.75, 0.6)
 	label2.AddText("control")
 	label2.AddText("region")
 	label2.SetBorderSize(0)
@@ -352,9 +363,12 @@ if __name__ == "__main__":
   	label2.SetTextColor(ROOT.kGray+2)
 	label2.Draw()
 
-	label3 = ROOT.TPaveText(0.78, 0.7, 0.9, 0.8)  
-	label3.AddText("signal")
-	label3.AddText("region")
+	label3 = ROOT.TPaveText(0.83, 0.7, 0.93, 0.8)  
+	text = label3.AddText("signal region")
+	#label3.AddText("signal")
+	#label3.AddText("region")
+	#label3.SetAllWith("Angle", 90)
+	text.SetTextAngle(90)
 	label3.SetBorderSize(0)
   	label3.SetFillColor(0)
   	label3.SetFillColorAlpha(ROOT.kWhite, 0.); 

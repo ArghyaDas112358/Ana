9608f40b870d312dc938ee968850a0da6d948fc0
Marc Huwiler <marc.huwiler@cern.ch>
2022-11-23 23:04:32 +0100

    Adding options and improving

--- GetEfficiency.py
+++ GetEfficiency.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
 
+from __future__ import division, print_function
+
 import os
 from ROOT import RDataFrame, TFile
 
 
-file = TFile.Open("../../data/TestEffCalc.root", "READ")
+if __name__ == "__main__": 
+	from argparse import ArgumentParser
 
-frame = RDataFrame(file.Get("ntuplizer/EffCalc"))
+	parser = ArgumentParser(description="GetEfficiency")
+	parser.add_argument("filename", action="store", type=str, default="", help="Name of file")
+	#parser.add_argument("-N", "--version", dest="iteration", action="store", type=int, default=0, help="Which iteration of inference")
+	#parser.add_argument("-i", "--input", dest="input", action="store", type=str, default="file.root", help="Number of points to be tested")
+	parser.add_argument("-o", "--object", dest="object", action="store", type=str, default="ntuplizer/EffCalc", help="Efficiency info object within file")
 
-n = frame.Sum("numSelected").GetValue()
+	options = parser.parse_args()
 
-N = frame.Sum("numTotal").GetValue()
 
-print "Number of selected events: {}/{}".format(n, N)
+	file = TFile.Open(options.filename, "READ")
 
-print "Efficiency: {}".format(n/N)
+	frame = RDataFrame(file.Get(options.object))
+
+	n = frame.Sum("numSelected").GetValue()
+
+	N = frame.Sum("numTotal").GetValue()
+
+	print("Number of selected events: {}/{}".format(n, N))
+
+	print("Efficiency: {}".format(n/N))
 

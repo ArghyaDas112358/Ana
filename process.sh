97a700ae1b0253d7e241566257a9cd9e2aa64215
Marc Huwiler <marc.huwiler@cern.ch>
2022-12-02 14:46:40 +0100

    Adding error handling process.sh script

--- process.sh
+++ process.sh
@@ -11,11 +11,29 @@ if [[ $# -ge 3 ]]; then
 	STOPEVT=${3}
 fi
 
-function processFull(){
+function processFull()
+{
 
 	. ApplyTFweight.sh $1 $2 $3
+
+	if [ $? -ne 0 ]; then
+		echo "Abort due to error. "
+		return $?
+	fi
+
 	. SelectTauCandidate.sh $1 $2
+
+	if [ $? -ne 0 ]; then
+		echo "Abort due to error. "
+		return $?
+	fi
+
 	. AddAnaBDT.sh $1 $2
+
+	if [ $? -ne 0 ]; then
+		echo "Abort due to error. "
+		return $?
+	fi
 
 }
 

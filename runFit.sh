ca0a7dafb0a3616c382f60aa65d0e737d28465a6
Marc Huwiler <marc.huwiler@cern.ch>
2022-12-09 14:29:01 +0100

    Working toy configuration

--- runFit.sh
+++ runFit.sh
@@ -1,5 +1,7 @@
 
 . fetchstuff.sh
-combine -M FitDiagnostics datacard.txt --saveWithUncertainties --saveOverallShapes --saveNormalizations --saveWorkspace --plots
+#combine -M FitDiagnostics datacard.txt --saveWithUncertainties --saveOverallShapes --saveNormalizations --saveWorkspace --plots
 #combine -M FitDiagnostics datacard.txt
-#combine -M MultiDimFit datacard.txt --saveWorkspace
+combine -M MultiDimFit datacard.txt --saveWorkspace
+#combine -M MultiDimFit higgsCombineTest.MultiDimFit.mH120.root --snapshotName MultiDimFit --saveWorkspace -t 100 --expectSignal 1. --toysNoSystematics
+combine -M FitDiagnostics higgsCombineTest.MultiDimFit.mH120.root --snapshotName MultiDimFit --saveWorkspace -t 100 --expectSignal 1. --toysNoSystematics

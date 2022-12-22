ae45a31a8bc5d2efda446d7aaef4c5a6eaa76685
Marc Huwiler <marc.huwiler@cern.ch>
2022-12-22 13:24:10 +0100

    Added option to chose step from which to process

--- process.sh
+++ process.sh
@@ -2,42 +2,56 @@
 
 ARG=${1}
 VERSION=""
+STEP=0
 STOPEVT=0
 
 if [[ $# -ge 2 ]]; then
 	VERSION=${2}
 fi
 if [[ $# -ge 3 ]]; then
-	STOPEVT=${3}
+	STEP=${3}
+fi
+if [[ $# -ge 4 ]]; then
+	STOPEVT=${4}
 fi
 
 function processFull()
 {
 
-	. ApplyTFweight.sh $1 $2 $3
+	if [ $4 -le 0 ]; then
+		. ApplyTFweight.sh $1 $2 $3
 
-	if [ $? -ne 0 ]; then
-		echo "Abort due to error. "
-		return $?
+		if [ $? -ne 0 ]; then
+			echo "Abort due to error. "
+			return $?
+		fi
 	fi
 
-	. SelectTauCandidate.sh $1 $2
+	if [ $4 -le 1 ]; then
+		. SelectTauCandidate.sh $1 $2
 
-	if [ $? -ne 0 ]; then
-		echo "Abort due to error. "
-		return $?
+		if [ $? -ne 0 ]; then
+			echo "Abort due to error. "
+			return $?
+		fi
 	fi
 
-	. AddAnaBDT.sh $1 $2
+	if [ $4 -le 2 ]; then
+		. AddAnaBDT.sh $1 $2
 
-	if [ $? -ne 0 ]; then
-		echo "Abort due to error. "
-		return $?
+		if [ $? -ne 0 ]; then
+			echo "Abort due to error. "
+			return $?
+		fi
+	fi
+
+	if [ $4 -ge 3 ]; then
+		echo "ERROR: Only 3 steps. Starting from step $4 will have no effect. "
 	fi
 
 }
 
 # Main 
 
-processFull $ARG $VERSION $STOPEVT
+processFull $ARG $VERSION $STOPEVT $STEP
 

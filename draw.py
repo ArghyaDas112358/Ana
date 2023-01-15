04b7c7241d116c0acec9bc68af1ad1e6334f323e
Marc Huwiler <marc.huwiler@cern.ch>
2023-01-16 00:41:10 +0100

    Added hack to plot pulls

--- draw.py
+++ draw.py
@@ -1,8 +1,11 @@
-import os, copy
-from ROOT import gStyle, TCanvas, TLegend, TH1F, gROOT, TFile, TColor
+import os, copy, ctypes
+from ROOT import gStyle, gROOT, TCanvas, TLegend, TH1F, TH1D, gROOT, TFile, TColor, THStack, Double_t
 #from common.officialStyle import officialStyle
 #from common.DisplayManager_postfit import DisplayManager
 #from common.DataMCPlot import *
+
+gROOT.LoadMacro("/Users/mhuwiler/coding/plugins/Drawing/RatioCanvas.h")
+from ROOT import RatioCanvas
 
 
 gROOT.SetBatch(True)
@@ -10,10 +13,31 @@ gStyle.SetOptStat(False)
 #gROOT.SetBatch(False)
 #officialStyle(gStyle)
 gStyle.SetOptTitle(0)
+textsize = 0.04
 
 def ensureDir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+
+def AtomicDraw(histo, name, options = ""): 
+    canv = TCanvas("canv", "canv", 800, 600)
+    histo.DrawCopy(options)
+    canv.Draw()
+    canv.Print(name)
+
+
+def ConvertGraphToHist(graph): 
+    h = TH1D(graph.GetName()+"_converted", graph.GetTitle(), graph.GetN(), graph.GetXaxis().GetXmin(), graph.GetXaxis().GetXmax())
+    err = TH1D(graph.GetName()+"_err", graph.GetTitle(), graph.GetN(), graph.GetXaxis().GetXmin(), graph.GetXaxis().GetXmax())
+    for i in range(0, graph.GetN()): 
+        x = ctypes.c_double()
+        y = ctypes.c_double()
+        graph.GetPoint(i, x, y);
+        h.Fill(x,y)
+        error = graph.GetErrorY(i) # Returns the average of high and low
+        err.SetBinContent(i, error)
+    return h , err
 
 
 #shape_file = '/work/ytakahas/work/analysis/CMSSW_10_2_10/src/rJpsi/anal/dev/datacard_MUSF_blind/tau_rhomass_unrolled_coarse_new.root'
@@ -91,6 +115,7 @@ for ftype in ['fit_s', 'fit_b']: #'prefit'
                 hist.SetMarkerSize(1)
 
             hist.SetLineWidth(3)
+            #hist.Sumw2()
         
             hists[ii] = copy.deepcopy(hist)
 
@@ -99,12 +124,14 @@ for ftype in ['fit_s', 'fit_b']: #'prefit'
             if ymax < hist.GetMaximum()*1.1: ymax = hist.GetMaximum()*1.1
 
             
-        canvas = TCanvas('canvas_' + ftype + '_' + cr)
+        name = 'canvas_' + ftype + '_' + cr
+        canvas = RatioCanvas(name, name, 800, 600)
 #        canvas.SetLogy()
-        legend = TLegend(  canvas.GetLeftMargin()+0.45, 
-                                1-canvas.GetTopMargin()-.25, 
-                                canvas.GetLeftMargin()+(1.-(canvas.GetLeftMargin()+canvas.GetRightMargin())),
-                                1-canvas.GetTopMargin())
+        pad = canvas.Upper()
+        legend = TLegend(  pad.GetLeftMargin()+0.45, # TODO: Implement GetMargin in RatioCanvas
+                                1-pad.GetTopMargin()-.25, 
+                                pad.GetLeftMargin()+(1.-(pad.GetLeftMargin()+pad.GetRightMargin())),
+                                1-pad.GetTopMargin())
 
         frame = TH1F('frame_' + ftype + '_' + cr, 'fname_' + ftype + '_' + cr, nbin, 15., 100.)
         frame.GetXaxis().SetTitle('Tau rhomasses unrolled bin ID')
@@ -118,7 +145,7 @@ for ftype in ['fit_s', 'fit_b']: #'prefit'
 #            frame.SetMinimum(0.8)
 
         frame.Draw()
-
+        
         hs = copy.deepcopy(hists['total_background'])
         hs.Add(copy.deepcopy(hists['total_signal']))
         hs.SetFillStyle(1)
@@ -145,16 +172,48 @@ for ftype in ['fit_s', 'fit_b']: #'prefit'
         hists['data'].Draw('epzsame')
         legend.Draw()
 
-        canvas.RedrawAxis()                
-        canvas.SaveAs('plots/combine/' + ftype + '_' + cr + '.gif')
+        #canvas.Upper().RedrawAxis()                # TODO: Implement RedraAxis in RatioCanvas
+
+        canvas.Lower()
+        #stack = THStack("stack", "Pulls")
+        #stack.Add(hists["signal"])
+        #stack.Add(hists["comb_bkg"])
+        #stack.Add(hists["part_bkg"])
+        #ratio = copy.deepcopy(hists["data"].GetHistogram())
+        ratio, err = ConvertGraphToHist(hists["data"])
+        ratio.Sumw2()
+        histfit = copy.deepcopy(hs)
+        histfit.Sumw2()
+        ratio.Add(histfit, -1.)
+        ratio.Divide(err)
+        ratio.SetMarkerStyle(8)
+        ratio.SetMarkerSize(0.5)
+        ratio.SetMarkerColor(1)
+        ratio.SetLineColor(1)
+        ratio.GetYaxis().SetRangeUser(-10., 10.)
+        ratio.GetYaxis().SetNdivisions(5)
+        #AtomicDraw(ratio, "plots/combine/ratio.png")
+        canvas.Lower()
+        ratio.Draw("PE SAME")
+        canvas.RemoveMiddleAxis()
+
+        frame.GetYaxis().SetLabelSize(textsize)
+        ratio.GetYaxis().SetLabelSize(textsize)
+        frame.GetYaxis().SetTitle("")
+        ratio.GetXaxis().SetLabelSize(textsize)
+        canvas.Draw()
+
+        #canvas.SaveAs('plots/combine/' + ftype + '_' + cr + '.gif') #TODO: Implement SaveAs in RatioCanvas
+        canvas.Print('plots/combine/' + ftype + '_' + cr + '.png')
+        canvas.Print('plots/combine/' + ftype + '_' + cr + '.pdf')
 
 
     # Checking consistency of shapes 
     newcanvas = TCanvas("newcanvas", "newcanvas", 800, 600)
-    legend = TLegend(  canvas.GetLeftMargin()+0.55, 
-                                1-canvas.GetTopMargin()-.15, 
-                                canvas.GetLeftMargin()+(1.-(canvas.GetLeftMargin()+canvas.GetRightMargin())),
-                                1-canvas.GetTopMargin())
+    legend = TLegend(  newcanvas.GetLeftMargin()+0.55, 
+                                1-newcanvas.GetTopMargin()-.15, 
+                                newcanvas.GetLeftMargin()+(1.-(newcanvas.GetLeftMargin()+newcanvas.GetRightMargin())),
+                                1-newcanvas.GetTopMargin())
     histSB = file.Get("shapes_fit_s/SB/bkg").DrawCopy("HIST E")
     histCR = file.Get("shapes_fit_s/CR/bkg").DrawCopy("HIST E SAME")
     histCR.SetLineColor(3)

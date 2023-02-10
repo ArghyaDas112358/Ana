fc91feb8a31e7f83e8acbebe57e75dc477da2784
Marc Huwiler <marc.huwiler@cern.ch>
2023-02-10 15:37:27 +0100

    Changing plot range and adding q2 cut

--- FileFlow.h
+++ FileFlow.h
@@ -192,7 +192,7 @@ namespace Ana
 			filemanager.AddItem("BkgBtoDstarDsstar", "/eos/home-m/mhuwiler/DoctoralThesis/Analysis/data/BkgDstarDsstarInclPrivateProdFirst.root", "ntuplizer/tree"); 
 		}
 
-		cut.emplace(std::make_pair("base", "(b_Ds_vprob>0.1) && (b_D0_vprob>0.1) && (mvaScore>-2.) && (B_mu_alpha > 1.)")); 
+		cut.emplace(std::make_pair("base", "(b_Ds_vprob>0.1) && (b_D0_vprob>0.1) && (mvaScore>-2.) && (B_mu_alpha > 1.) && (q_B_q2 > 6.)")); // TODO: change from B_mu_alpha to picked B candidate 
 		cut.emplace(std::make_pair("SR", cut["base"]+TCut(TString::Format("mvaScore>=%f", mvaCutSR)))); 
 		cut.emplace(std::make_pair("SB", cut["base"]+TCut(TString::Format("(mvaScore >= %f) && (mvaScore < %f)", mvaCutSB, mvaCutSR)))); 
 		cut.emplace(std::make_pair("CR", cut["base"]+TCut(TString::Format("(mvaScore >= %f) && (mvaScore < %f)", mvaCutCR, mvaCutSB)))); 
@@ -219,7 +219,7 @@ namespace Ana
 		binning = {{"tau_rhomass1", {"", ";#rho_{12} mass [GeV]", 20, 0., 1.5}},
 				{"tau_rhomass2", {"", ";#rho_{12} mass [GeV]", 20, 0., 1.5}},
 				{"B_m", {"", ";B mass [GeV]", 40, 0., 6.}},
-				{"B_q2", {"", ";B mass [GeV]", 40, 0., 20.}},
+				{"B_q2", {"", ";B mass [GeV]", 40, 0., 12.}},
 		}; 
 
 	}

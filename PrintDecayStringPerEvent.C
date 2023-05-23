d009934df09b510f5c9b813dc67f524702f4f4a9
Marc Huwiler <marc.huwiler@cern.ch>
2023-05-23 13:38:52 +0200

    Add printing for Dstar decay products

--- PrintDecayStringPerEvent.C
+++ PrintDecayStringPerEvent.C
@@ -10,10 +10,16 @@ void PrintDecayString(TTree *tree)
 	std::string *decaystring1 = new std::string(); 
 	std::string *decaystring2 = new std::string(); 
 	std::string *decaystring3 = new std::string(); 
+	std::string *decaystringK = new std::string(); 
+	std::string *decaystringpi = new std::string(); 
+	std::string *decaystringspi = new std::string(); 
 
 	tree->SetBranchAddress("pttau_tau_gen1str", &decaystring1); 
 	tree->SetBranchAddress("pttau_tau_gen2str", &decaystring2); 
 	tree->SetBranchAddress("pttau_tau_gen3str", &decaystring3); 
+	tree->SetBranchAddress("D0_genkstr", &decaystringK); 
+	tree->SetBranchAddress("D0_genpistr", &decaystringpi); 
+	tree->SetBranchAddress("Dstar_genpistr", &decaystringspi); 
 
 	tree->SetBranchAddress("genstring", &decaystring); 
 
@@ -30,10 +36,14 @@ void PrintDecayString(TTree *tree)
 
 		std::cout << *decaystring << std::endl; 
 
-		std::cout << *decaystring1 << std::endl; 
-		std::cout << *decaystring2 << std::endl; 
-		std::cout << *decaystring3 << std::endl; 
+		std::cout << "K: " << *decaystringK << std::endl; 
+		std::cout << "pi: " << *decaystringpi << std::endl; 
+		std::cout << "spi: " << *decaystringspi << std::endl; 
+		std::cout << "pi1: " << *decaystring1 << std::endl; 
+		std::cout << "pi2: " << *decaystring2 << std::endl; 
+		std::cout << "pi3: " << *decaystring3 << std::endl; 
 	}
+	std::cout << std::endl << "Analyzed " << max << " events. " << std::endl; 
 }
 
 

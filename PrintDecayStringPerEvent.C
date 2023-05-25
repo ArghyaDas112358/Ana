fe4ca15c0c5af61d18153caf8468fc30fd0ce478
Marc Huwiler <marc.huwiler@cern.ch>
2023-05-25 10:38:34 +0200

    Fix to prevent printing several times same event

--- PrintDecayStringPerEvent.C
+++ PrintDecayStringPerEvent.C
@@ -23,7 +23,7 @@ void PrintDecayString(TTree *tree)
 
 	tree->SetBranchAddress("genstring", &decaystring); 
 
-	int max = std::max(10, static_cast<int>(tree->GetEntries())); 
+	int max = std::min(10, static_cast<int>(tree->GetEntries())); 
 	for (int i=0; i<max; i++) 
 	{
 		tree->GetEntry(i);

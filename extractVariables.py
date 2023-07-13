3df081156e945dd13b8c20b39a67024f1ace5a71
Marc Huwiler <marc.huwiler@cern.ch>
2023-07-13 18:35:16 +0200

    Fixed extractVariable to return the type

--- extractVariables.py
+++ extractVariables.py
@@ -6,9 +6,13 @@ import ROOT
 def extractVariables(variables): 
 	vec = ROOT.vector("TString")()
 	for item in variables : 
-		vec.push_back(ROOT.TString(item[0]))
+		vec.push_back(ROOT.TString(item[0]+"/"+item[1]))
 		print(item[0])
 	return vec
+
+def getVariables(): 
+	from anaMVA.BDTvariables import features_save
+	extractVariables(features_save)
 
 
 if __name__ == "__main__": 
@@ -16,5 +20,7 @@ if __name__ == "__main__":
 	from anaMVA.BDTvariables import features_save
 
 	extractVariables(features_save)
+
+	getVariables()
 
 

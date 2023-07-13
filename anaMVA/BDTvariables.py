c867e27e6774f8672e9e8f583a130729e75c5e39
Marc Huwiler <marc.huwiler@cern.ch>
2023-07-13 15:52:12 +0200

    Moved training variables to an external file

---
+++ anaMVA/BDTvariables.py
@@ -0,0 +1,18 @@
+
+
+
+
+features_save = [("D0_pt", "F"), ("D0_eta", "F"), ("D0_phi", "F"), ("D0_vprob", "F"), ("D0_fl", "F"), ("D0_fsig", "F"),
+	("Dstar_pt", "F"), ("Dstar_eta", "F"), ("Dstar_phi", "F"), ("Dstar_vprob", "F"), ("Dstar_fl", "F"), ("Dstar_fsig", "F"), 
+	("D0_lip", "F"), ("D0_lipsig", "F"), ("D0_pvip", "F"), ("Dstar_lip", "F"), ("Dstar_lipsig", "F"), ("Dstar_pvip", "F"), 
+	("b_tau_pt", "F"), ("b_tau_eta", "F"), ("b_tau_phi", "F"), ("b_tau_fl", "F"), ("b_tau_fsig", "F"), ("b_tau_vprob", "F"), 
+	("b_tau_lip", "F"), 
+	#("b_tau_lips", "F"), 
+	("b_tau_pvip", "F"), ("b_tau_pvipsig", "F"), ("b_tau_alpha", "F"), ("b_tau_legacyMaxdr", "F"), 
+	("b_tau_pi1pt", "F"), ("b_tau_pi1eta", "F"), ("b_tau_pi1phi", "F"), 
+	("b_tau_pi2pt", "F"), ("b_tau_pi2eta", "F"), ("b_tau_pi2phi", "F"), 
+	("b_tau_pi3pt", "F"), ("b_tau_pi3eta", "F"), ("b_tau_pi3phi", "F"), ("b_tau_sumdnn", "F"), 
+	#("BsDstarTauNu_k_charge", "F"), ("BsDstarTauNu_pi_charge", "F"), ("BsDstarTauNu_spi_charge", "F"), 
+	] #("BsDstarTauNu_mu1_vx", "F"), ("BsDstarTauNu_mu1_vy", "F"), ("BsDstarTauNu_tau_pi1_charge", "F"), ("BsDstarTauNu_tau_pi2_charge", "F"), ("BsDstarTauNu_tau_pi3_charge", "F"), ("BsDstarTauNu_mu1_q", "I"), ("BsDstarTauNu_tau_q", "I"), ("BsDstarTauNu_mu1_vz", "F")
+
+

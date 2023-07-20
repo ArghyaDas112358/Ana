55e5d058266a22957a395c29f8216d947ff04087
Marc Huwiler <marc.huwiler@cern.ch>
2023-07-20 16:07:55 +0200

    Cleaning Efficiencies script

--- Efficiencies.py
+++ Efficiencies.py
@@ -2,25 +2,15 @@
 from __future__ import division, print_function
 
 import ROOT
-import os
-import math
-import collections
 import copy
 import json
 from argparse import ArgumentParser
-from ROOT import TCanvas, TH1D, TPad, TLegend, THStack, RDataFrame
 from uncertainties import ufloat
 from uncertainties.umath import * 
 
 
-#ROOT.gROOT.LoadMacro("/Users/mhuwiler/coding/plugins/libFunctions.C+")#ROOT.gROOT.LoadMacro("/eos/home-m/mhuwiler/plugins/libFunctions.C")
-#ROOT.gROOT.LoadMacro("/Users/mhuwiler/coding/plugins/Drawing/ExperimentSpecificLayer.C")
-#ROOT.gROOT.LoadMacro("/eos/home-m/mhuwiler/plugins/FileManager/CFileManager.C")
-#ROOT.gROOT.LoadMacro("/Users/mhuwiler/coding/plugins/Drawing/CMS/tdrstyle.C")
 ROOT.gROOT.LoadMacro("FileFlow.h")
-ROOT.gROOT.LoadMacro("/Users/mhuwiler/coding/plugins/Drawing/RatioCanvas.h")
-#ROOT.setTDRStyle()
-#import CMS_lumi
+
 from ROOT import Ana
 
 from libEfficiencies import getEffFromInfo, getGenmatchingEff, DumpEffs, ReadEffs, FormatLatex
@@ -73,8 +63,6 @@ if __name__ == "__main__":
 	template = "{} & ${}$ & ${}$ & ${}$ & ${}$ & ${:fL}$ & ${}$ \\\\\n" #{:.1e} "{} & ${:.{precision}eL}$ & ${:.{precision}eL}$ & ${:.{precision}eL}$ & ${:fL}$ & ${}$ \\\\\n"
 
 	numberafterselection = options.target
-
-	namedict = {"Sig":"$B^0\\rightarrow D^{*-}\\tau^+\\nu_\\tau$", "BkgDstarDs": "$B^0\\rightarrow D^{*-}D_{s}^+$", "BkgDstarDsstar": "$B^0\\rightarrow D^{*-}D_{s}^{*+}$", "BkgBuDXc": "$B^+\\rightarrow D_{(s)}^{(*)}D_{(s)}^{(*)}$", "BkgB0DD": "$B^0\\rightarrow D_{(s)}^{(*)}D_{(s)}^{(*)}$", "B0toDstarD0K": "$B^0\\rightarrow D^{*}D^{0}K$", "B0toDstarDs1": "$B^{0}\\rightarrow D^{*}D_{s1}$", "B0toDstarDs": "$B^0\\rightarrow D^{*-}D_{s}^+$", "B0toDstarDsstar": "$B^0\\rightarrow D^{*-}D_{s}^{*+}$", "BkgDstara1": "$B^0\\rightarrow D^{*-}a_{1}^+$"} #{"Sig":"$\\smash{\\myoverset{\\brabar}{B}^0\\rightarrow D^{*\\mp}\tau^\\pm\\myoversetnu{\\brabar}{\\nu}_\\tau}$", "BkgDstarDs": "$\\smash{\\myoverset{\\brabar}{B}^0\\rightarrow D^{*\\mp}D_{s}^\\pm}$", "BkgDstarDsstar": "$\\smash{\\myoverset{\\brabar}{B}^0\\rightarrow D^{*\\mp}D_{s}^{*\\pm}}$"} # "BkgBuDXc": "$B^+\\rightarrow D_{(s)}^{(*)}D_{(s)}^{(*)}\\pi^+X$", "BkgB0DD": "$B^0\\rightarrow D_{(s)}^{(*)}D_{(s)}^{(*)}X$"
 
 	Nexpected = {}
 

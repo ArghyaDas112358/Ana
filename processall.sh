767d699954c0a49786990c02a95e9160e9bcd159
Marc Huwiler <marc.huwiler@cern.ch>
2023-08-02 11:56:35 +0200

    Improving by throwing error if no version

--- processall.sh
+++ processall.sh
@@ -2,7 +2,12 @@
 
 VERSION=${1}
 
-SAMPLELIST=(Sig SigTrain SigTest B0toDstarDs B0toDstarDsstar B0toDstarDs1)
+SAMPLELIST=( B0toDstarDs B0toDstarDsstar B0toDstarDs1 B0toDstarD0K B0toDstarD0Kstar ) #SigTrain  #Sig SigTest 
+
+if [[ "${VERSION}" == "" ]]; then
+	echo "ERROR: no version provided. Please provide the version of the samples as argument."
+	return $?
+fi
 
 for ITEM in "${SAMPLELIST[@]}"; do
 	. process.sh $ITEM $VERSION

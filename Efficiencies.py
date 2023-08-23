1bbcbc90b40bb8a048789291b6ac8b1d586cbf76
Marc Huwiler <marc.huwiler@cern.ch>
2023-08-23 16:28:37 +0200

    Computing efficiency before offline analysis

--- Efficiencies.py
+++ Efficiencies.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
 			offlineeff = getOfflineEff(vec)
 			print(offlineeff)
 
-			N = options.lumi*constants["sigmabb"]*constants["fB0"]*2*forcedbr[sample]*constants["BrDstar2D0pi"]*constants["BrD02Kpi"]*1000*filtereffs[sample]*eff*offlineeff*genmatcheff*customeff
+			N = options.lumi*constants["sigmabb"]*constants["fB0"]*2*forcedbr[sample]*constants["BrDstar2D0pi"]*constants["BrD02Kpi"]*1000*filtereffs[sample]*eff*offlineeff #*genmatcheff*customeff
 			Nexpected[sample] = N
 			#print(10000./eff)
 			print("\tN expected for {}: {} (filter eff: {}, ana eff: {}, genmatch eff: {}, number requested: {})".format(sample, N, filtereffs[sample], eff, genmatcheff, numberafterselection/eff))

d200f6047e682ece0a31f3b38aacaf3f90d73dcf
Marc Huwiler <marc.huwiler@cern.ch>
2023-09-29 09:20:54 +0200

    Fixing WS parameter position

--- processall.sh
+++ processall.sh
@@ -64,7 +64,7 @@ done
 
 for ITEM in "${WSPROCLIST[@]}"; do
 	#echo $ITEM
-	. process.sh $ITEM $VERSION $STAGE 1
+	. process.sh $ITEM $VERSION $STAGE 0 1
 	RETURNCODE=$?
 	if [ $RETURNCODE -ne 0 ]; then 
 		echo $ITEM >> failprocessing.txt

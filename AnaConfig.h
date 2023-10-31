b459f9f89ce75c089bc095dfea2ea63e9c4e82a0
Marc Huwiler <marc.huwiler@cern.ch>
2023-10-31 16:14:09 +0100

    First version of json reading in C++ code

---
+++ AnaConfig.h
@@ -0,0 +1,96 @@
+#ifndef FileFlow_h
+#define FileFlow_h
+#include "plugins/FileManager/CFileManager.C"
+#include <string>
+#include "TString.h"
+#include "TCut.h"
+#include <unordered_map>
+#include "SampleData.C"
+#include "ROOT/RDataFrame.hxx"
+#define BOOST_JSON_STACK_BUFFER_SIZE 1024
+#include <boost/json/src.hpp> // Currently not used, remove?
+#include <json/value.h>
+#include <json/json.h>
+//R__ADD_LIBRARY_PATH($FOODIR) // if needed
+R__LOAD_LIBRARY(/opt/local/lib/libjsoncpp.dylib) // Load the library
+#include <fstream>
+
+
+//gROOT->Load("/opt/local/lib/libjsoncpp.dylib")
+
+
+namespace Ana 
+{
+
+	FileManager filemanager; 
+
+	std::unordered_map<std::string, Int_t> colorold; 
+
+	std::unordered_map<std::string, ROOT::RDF::TH1DModel> binning; 
+
+	std::unordered_map<std::string, SampleData> samples; 
+
+
+	void LoadRegions(const TString& path) 
+	{
+		std::ifstream file(path+"/Info.json", std::ifstream::binary);
+		Json::Value regions;
+		file >> regions;
+
+		for (auto item : regions)
+		{
+			std::cout << item << " " << std::endl; 
+		}
+
+	}
+
+
+	void Load(const TString& regions)
+	{
+	
+		colorold = {{"Sig", 2}, {"BkgDstarDs", 3}, {"BkgDstarDsstar", 8}, {"BkgDstara1", 4}, {"dataD2WS", 6}, {"dataD2TauWS", 7}, {"other", 9}, {"yetanother", 1}}; // Legacy color scheme 
+
+
+		Int_t nBins = 20; 
+
+		binning = {{"b_tau_rhomass1", {"", "#rho_{12} mass;Invariant m_{#rho} [GeV];Counts", nBins, 0., 1.5}},
+				{"b_tau_rhomass2", {"", "rho_{23} mass;Invariant m_{#rho} [GeV];Counts", nBins, 0., 1.5}},
+				{"b_B_m", {"", "B mass;Reconstructed m_{B} [GeV];Counts", nBins, 2., 6.}},
+				{"b_B_q2", {"", "q2;q^{2} [GeV];Counts", nBins, 0., 12.}},
+				//{"B_m", {"", ";B mass [GeV];Counts", nBins, 0., 6.}},
+				//{"B_q2", {"", ";B mass [GeV];Counts", nBins, 0., 12.}},
+				//{"tau_rhomass1", {"", ";#rho_{12} mass [GeV];Counts", nBins, 0., 1.5}},
+				//{"tau_rhomass2", {"", ";#rho_{12} mass [GeV];Counts", nBins, 0., 1.5}},
+				{"b_B_proper_xi_rho1", {"", "#tau mass;Reconstructed m_{#tau} [GeV];Counts", nBins, -1.1, 1.1}},
+				{"b_B_proper_xi_rho2", {"", "#tau mass;Reconstructed m_{#tau} [GeV];Counts", nBins, -1.1, 1.1}},
+				{"b_tau_proper_alpha_rho1_pi", {"", "#tau mass;Reconstructed m_{#tau} [GeV];Counts", nBins, -1.1, 1.1}},
+				{"b_tau_proper_alpha_rho2_pi", {"", "#tau mass;Reconstructed m_{#tau} [GeV];Counts", nBins, -1.1, 1.1}},
+				{"b_tau_proper_theta_rho1", {"", "#tau mass;Reconstructed m_{#tau} [GeV];Counts", nBins, -1.1, 1.1}},
+				{"b_tau_proper_theta_rho2", {"", "#tau mass;Reconstructed m_{#tau} [GeV];Counts", nBins, -1.1, 1.1}},
+		}; 
+
+		// Complete binning with default
+		std::unordered_map<std::string, ROOT::RDF::TH1DModel> defaultbinning = { 
+			#include "VariableDefinitions.ccf" 
+		}; 
+		for (auto item : defaultbinning) 
+		{
+			if (binning.find(item.first) == binning.end()) 
+			{
+				binning[item.first] = item.second; 
+			}
+		}
+
+		samples = InitSamples(); 
+
+		if (regions != "") LoadRegions(regions); 
+
+	}
+
+}
+
+
+
+
+#endif
+

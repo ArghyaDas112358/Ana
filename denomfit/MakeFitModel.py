4d22cd2d3cb4dd25ad18b147e8f4ef95aadb55b0
Marc Huwiler <marc.huwiler@cern.ch>
2024-01-03 15:54:41 +0100

    Use larger file for fitting

--- denomfit/MakeFitModel.py
+++ denomfit/MakeFitModel.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
 	#sigfile = ROOT.TFile.Open("B0toDstarDs_tauDNN.root", "READ")
 	#sig = cp.deepcopy(RDataFrame(sigfile.Get("ntuplizer/tree")).Histo1D(("sig", "sig", 40, 0., 20.), variable).GetPtr())
 
-	datafile = ROOT.TFile.Open("../../../data/v7.01/dataD1.root", "READ")
-	data = RDataFrame(datafile.Get("ntuplizer/tree")).Filter("pttau_B_m>0.&&pttau_B_nmu<1&&pttau_B_nh<1&&pttau_B_ne<1&&(pttau_B_npi0+pttau_B_ngamma)<2").Histo1D(examplehist, variable)
+	datafile = ROOT.TFile.Open("../../../data/v7.01/dataD1-2.root", "READ")
+	data = RDataFrame(datafile.Get("ntuplizer/tree")).Filter("pttau_B_m>0.&&pttau_B_nmu<1&&pttau_B_nh<1&&pttau_B_ne<1&&(pttau_B_npi0+pttau_B_ngamma)<10").Histo1D(examplehist, variable)
 
 	var = RooRealVar(variable, variable, 0., 100.)
 	fitspace = RooArgSet(var)

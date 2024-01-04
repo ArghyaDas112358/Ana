1f110fb51b4b725295df0869babbb5aaaf4d9154
Marc Huwiler <marc.huwiler@cern.ch>
2024-01-05 00:37:08 +0100

    Plotting signal component

--- denomfit/PlotDenomFit.py
+++ denomfit/PlotDenomFit.py
@@ -5,7 +5,8 @@ from __future__ import division, print_function
 
 
 import ROOT
-from ROOT import RooRealVar, RooDataHist, RooArgSet, RooWorkspace, RooExponential, RooCrystalBall, RooAddPdf, RooArgList
+from ROOT import RooRealVar, RooDataHist, RooArgSet, RooWorkspace, RooExponential, RooCrystalBall, RooAddPdf, RooArgList, TColor
+from ROOT.RooFit import Components, LineColor, FillColor, DrawOption, LineWidth, Name
 
 
 if __name__ == "__main__":
@@ -28,7 +29,11 @@ if __name__ == "__main__":
 
 	datahist.plotOn(frame)
 
-	model.getPdf("bin0").plotOn(frame) #model.plotOn(frame)
+	totalpdf = model.getPdf("bin0") #model.plotOn(frame)
+
+	totalpdf.plotOn(frame)
+
+	totalpdf.plotOn(frame, Components("shapeSig_sig_bin0"), LineColor(ROOT.kRed), DrawOption("LF"), FillColor(TColor.GetColorTransparent(ROOT.kRed-7,0.3)), LineWidth(1), Name("Signal"))
 
 	canvas = ROOT.TCanvas("canvas", "canvas", 800, 600)
 	frame.Draw()
--- denomfit/runDenomFit.sh
+++ denomfit/runDenomFit.sh
@@ -4,4 +4,4 @@
 python3 MakeFitModel.py
 combine -M MultiDimFit datacard.txt --saveWorkspace --algo singles --saveFitResult
 PostFitShapesFromWorkspace -w higgsCombineTest.MultiDimFit.mH120.root --output postFitPlots.root --fitresult multidimfitTest.root:fit_mdf --postfit
-#python3 PlotDenomFit.py
+python3 PlotDenomFit.py

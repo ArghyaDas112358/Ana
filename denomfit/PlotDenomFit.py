77c780017ac0b1d37a041c6513d482082f405241
Marc Huwiler <marc.huwiler@cern.ch>
2024-01-05 00:43:30 +0100

    Adding combinatorial background component

--- denomfit/PlotDenomFit.py
+++ denomfit/PlotDenomFit.py
@@ -6,7 +6,7 @@ from __future__ import division, print_function
 
 import ROOT
 from ROOT import RooRealVar, RooDataHist, RooArgSet, RooWorkspace, RooExponential, RooCrystalBall, RooAddPdf, RooArgList, TColor
-from ROOT.RooFit import Components, LineColor, FillColor, DrawOption, LineWidth, Name
+from ROOT.RooFit import Components, LineColor, FillColor, DrawOption, LineWidth, LineStyle, Name
 
 
 if __name__ == "__main__":
@@ -34,6 +34,7 @@ if __name__ == "__main__":
 	totalpdf.plotOn(frame)
 
 	totalpdf.plotOn(frame, Components("shapeSig_sig_bin0"), LineColor(ROOT.kRed), DrawOption("LF"), FillColor(TColor.GetColorTransparent(ROOT.kRed-7,0.3)), LineWidth(1), Name("Signal"))
+	totalpdf.plotOn(frame, Components("shapeBkg_comb_bin0"), LineColor(ROOT.kBlue+3), LineWidth(1), LineStyle(ROOT.kDotted), Name("Combinatorial"))
 
 	canvas = ROOT.TCanvas("canvas", "canvas", 800, 600)
 	frame.Draw()

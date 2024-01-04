b9c03694df311fe0f5fcf1f882e2b8388471756f
Marc Huwiler <marc.huwiler@cern.ch>
2024-01-04 22:39:09 +0100

    Plotting denominator working

--- denomfit/PlotDenomFit.py
+++ denomfit/PlotDenomFit.py
@@ -11,24 +11,24 @@ from ROOT import RooRealVar, RooDataHist, RooArgSet, RooWorkspace, RooExponentia
 if __name__ == "__main__":
 	print("Start plotting")
 
-	fitfile = ROOT.TFile.Open("workspace.root", "READ")
+	fitfile = ROOT.TFile.Open("higgsCombineTest.MultiDimFit.mH120.root", "READ")
 
 	workspace = fitfile.Get("w")
 
 	workspace.loadSnapshot("MultiDimFit")
 
 
-	model = workspace.pdf("dcbPdf") #model_s
+	model = workspace.pdf("model_s") #model_s
 
 	# Creating the RooFit plot
 	var = workspace.var("pttau_B_m")
 	frame = var.frame()
 
-	model.plotOn(frame) #model.getPdf("bin0").plotOn(frame)
-
 	datahist = workspace.data("data_obs")
 
 	datahist.plotOn(frame)
+
+	model.getPdf("bin0").plotOn(frame) #model.plotOn(frame)
 
 	canvas = ROOT.TCanvas("canvas", "canvas", 800, 600)
 	frame.Draw()

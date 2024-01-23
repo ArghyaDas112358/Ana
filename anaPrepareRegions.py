862130087af8410c8fdea30d400ac9927f82f9ec
Marc Huwiler <marc.huwiler@cern.ch>
2024-01-23 11:51:41 +0100

    Cleaning up code

--- anaPrepareRegions.py
+++ anaPrepareRegions.py
@@ -116,57 +116,26 @@ def GetBaseName(samplename):
 	return sample
 
 
-ROOT.gInterpreter.Declare(''' 
-	namespace Ana 
-	{
-		std::vector<std::string>& purgeColumns(std::vector<std::string> &&columns, const std::vector<std::string>& blacklist)
-		{
-	   			// a lambda that checks if `s` is in the blacklist
-	   			auto is_blacklisted = [&blacklist](const std::string &s)  { return std::find(blacklist.begin(), blacklist.end(), s) != blacklist.end(); };
-
-	   			// removing elements from std::vectors is not pretty, see https://en.wikipedia.org/wiki/Erase%E2%80%93remove_idiom
-	   			columns.erase(std::remove_if(columns.begin(), columns.end(), is_blacklisted), columns.end());
-
-	   			return columns; 
-		}
-	}
-''')
-
-
 def SaveDataframe(tosave, filename, objectname = "tree", blacklist = anaConfig.blacklist):
 	ROOT.fixStringVariables(tosave)
 	tosave.Snapshot(objectname, filename, Ana.purgeColumns(tosave.GetColumnNames(), anaConfig.blacklist))
 	print("Written frame to {} in {}".format(objectname, filename))
+	
 
+# ROOT.gInterpreter.Declare('''
+# 	std::vector<std::string>& purgeColumns(std::vector<std::string> &&columns)
+# 	{
+# 		const std::vector<std::string> blacklist = {"v_taucandidates", "b_tau"};
+# 			// a lambda that checks if `s` is in the blacklist
+# 			auto is_blacklisted = [&blacklist](const std::string &s)  { return std::find(blacklist.begin(), blacklist.end(), s) != blacklist.end(); };
 
-ROOT.gInterpreter.Declare(''' 
-	template<typename T>
-	void fixStringVariables(T &dataframe)
-	{
-		#include "stringbranches.gcf"
+# 			// removing elements from std::vectors is not pretty, see https://en.wikipedia.org/wiki/Erase%E2%80%93remove_idiom
+# 			columns.erase(std::remove_if(columns.begin(), columns.end(), is_blacklisted), columns.end());
 
-		for (auto branch : stringbranches) // Hack to fix string branche 
-		{
-			dataframe = dataframe.Redefine(branch, [](const ROOT::RVec<std::string> &v) {return std::vector<std::string>(v.begin(), v.end());}, {branch}); 
-		}
-	}
-	'''
-) # Move this somewehre else
-
-ROOT.gInterpreter.Declare('''
-	std::vector<std::string>& purgeColumns(std::vector<std::string> &&columns)
-	{
-		const std::vector<std::string> blacklist = {"v_taucandidates", "b_tau"};
-			// a lambda that checks if `s` is in the blacklist
-			auto is_blacklisted = [&blacklist](const std::string &s)  { return std::find(blacklist.begin(), blacklist.end(), s) != blacklist.end(); };
-
-			// removing elements from std::vectors is not pretty, see https://en.wikipedia.org/wiki/Erase%E2%80%93remove_idiom
-			columns.erase(std::remove_if(columns.begin(), columns.end(), is_blacklisted), columns.end());
-
-			return columns; 
-	}
-	'''
-) 
+# 			return columns; 
+# 	}
+# 	'''
+# ) 
 
 
 def SaveRegions(frames, path, update = False, objectinfile="tree"): 
@@ -184,7 +153,7 @@ def SaveRegions(frames, path, update = False, objectinfile="tree"):
 			filename = "{}/{}_{}.root".format(path, item, key)
 			frame = frames[item][key]
 			ROOT.fixStringVariables(frame)
-			frame.Snapshot(objectinfile, filename, ROOT.purgeColumns(frame.GetColumnNames()), snapshotOptions)
+			frame.Snapshot(objectinfile, filename, Ana.old.purgeColumns(frame.GetColumnNames()), snapshotOptions)
 
 			info[item][key] = (filename, objectinfile)
 

13e58956bb938c9b7d6d64609ee5d068d63dd938
Marc Huwiler <marc.huwiler@cern.ch>
2024-03-14 15:44:11 +0100

    Updating runFit.sh script

--- runFit.sh
+++ runFit.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 
-. fetchstuff.sh
+#. fetchstuff.sh
 #combine -M FitDiagnostics datacard.txt --saveWithUncertainties --saveOverallShapes --saveNormalizations --saveWorkspace --plots
 #combine -M FitDiagnostics datacard.txt
 #combine -M MultiDimFit datacard.txt --saveWorkspace
+
+# Exporting the fit distributions
 combine -M MultiDimFit datacardGeneratedSimpleHacked.txt --saveWorkspace  --saveFitResult
 PostFitShapesFromWorkspace -w higgsCombineTest.MultiDimFit.mH120.root --output postFitPlots.root --fitresult multidimfitTest.root:fit_mdf --postfit
 #combine -M MultiDimFit higgsCombineTest.MultiDimFit.mH120.root --snapshotName MultiDimFit --saveWorkspace -t 100 --expectSignal 1. --toysNoSystematics

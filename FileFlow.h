4b2ce8900aa6ded0106096636bc251a7b75d7978
Marc Huwiler <marc.huwiler@cern.ch>
2025-04-15 15:22:36 +0200

    Cleaning final cuts

--- FileFlow.h
+++ FileFlow.h
@@ -684,12 +684,13 @@ namespace Ana
 		if (!denominator) 
 		{
 			cut.emplace(std::make_pair("training", "(b_B_fsig>2.)"));
-			cut.emplace(std::make_pair("bare", "(b_B_fsig>2.)&&b_tau_vprob>0.1&&(b_tau_fsig>3.)&&(b_tau_alpha>0.)&&(b_B_r<1.)&&(b_B_mu_alpha>1.)&&(b_tau_rhomass_max>0.5)&&(b_tau_m<1.7)&&(b_B_m>3.)&&(b_B_m<5.28)")); // (b_tau_m<1.7)&&(b_B_m>3.)&&(b_B_m<5.5)&&&&(b_tau_min_dr_mu>0.5)&&mvaScore>0.7
-			cut.emplace(std::make_pair("base", TString::Format("(b_B_fsig>2.)&&(b_tau_fsig>3.)&&(b_tau_alpha>0.)&&(b_B_r<1.)&&(b_B_mu_alpha>1.)&&(b_tau_rhomass_max>0.5)&&(b_tau_m<1.7)&&(b_B_m>3.)&&(b_B_m<5.28)&&(%s)&&%s", ABCDconfig.cutYh.c_str(), ABCDconfig.cutXh.c_str()))); // (b_tau_sumdnn>2.)b_B_nmu<1&&b_B_ne<1&&b_B_nh<1&&b_tau_vprob>0.1(b_tau_m<1.7)&&(b_B_m>3.)&&(b_B_m<5.5)&&&&(b_tau_min_dr_mu>0.5)&&mvaScore>0.7
+			cut.emplace(std::make_pair("bare", "(b_B_fsig>2.)&&(b_tau_fsig>3.)&&b_tau_vprob>0.1&&(b_tau_alpha>0.)&&(b_B_r<1.)&&(b_B_mu_alpha>1.)&&(b_tau_rhomass_max>0.5)&&(b_tau_m<1.7)&&(b_B_m>3.)&&(b_B_m<5.28)")); // (b_tau_m<1.7)&&(b_B_m>3.)&&(b_B_m<5.5)&&&&(b_tau_min_dr_mu>0.5)&&mvaScore>0.7
+			cut.emplace(std::make_pair("base", cut["bare"]+TCut(TString::Format("(%s)&&%s", ABCDconfig.cutYh.c_str(), ABCDconfig.cutXh.c_str())))); // (b_tau_sumdnn>2.)b_B_nmu<1&&b_B_ne<1&&b_B_nh<1&&b_tau_vprob>0.1(b_tau_m<1.7)&&(b_B_m>3.)&&(b_B_m<5.5)&&&&(b_tau_min_dr_mu>0.5)&&mvaScore>0.7
 			// &&b_B_nmu<1&&b_B_ne<1&&b_B_nh<3 &&(b_tau_min_dr_mu>0.5)&&(b_tau_alpha>0)Dstar_vprob>0.1 (b_tau_m<1.7)&&(mvaScore>0.3)&&(b_tau_min_dr_mu>0.5)&&(b_tau_alpha>0)&&(b_B_fsig>2.)&&b_B_m>3.&&b_B_nmu<1&&b_B_nh<1&&b_B_ne<1&&(b_B_npi0+b_B_ngamma)<2&&Dstar_vprob>0.1       (b_tau_m<1.7)&&(mvaScore>0.7)&&(b_tau_min_dr_mu>0.5)&&(b_tau_alpha>0)&&(D0_fsig>3.)&&(b_B_m>3.)     (b_tau_m<1.7)&&(b_tau_min_dr_mu>0.5)&&(b_tau_alpha>0)&&(D0_fsig>3.)&&(b_B_m>3.) //&&(b_tau_min_dr_mu>0.5)&&(b_tau_min_dr_e>0.5)//&& (b_B_q2 > 6.) // (b_Ds_vprob>0.1) && (b_D0_vprob>0.1) && (mvaScore>-2.) && (b_B_mu_alpha > 1.) &&((b_tau_m_KKpi1>2.)||(b_tau_m_KKpi1<1.9))&&((b_tau_m_KKpi2>2.)||(b_tau_m_KKpi2<1.9)) &&(b_tau_min_dr_mu>0.5)&&(b_tau_min_dr_e>0.5)&&(b_tau_alpha>0) (b_tau_m<1.7)&&(b_tau_alpha>0)&&(D0_fsig>3.)&&(b_B_m>3.)&&(b_tau_vtx4trkProb<0.2)
 			cut.emplace(std::make_pair("SR", cut["base"]+TCut(TString::Format("mvaScore>=%f", mvaCutSR)))); 
 			cut.emplace(std::make_pair("SB", cut["base"]+TCut(TString::Format("(mvaScore >= %f) && (mvaScore < %f)", mvaCutSB, mvaCutSR)))); 
 			cut.emplace(std::make_pair("CR", cut["base"]+TCut(TString::Format("(mvaScore >= %f) && (mvaScore < %f)", mvaCutCR, mvaCutSB)))); 
+			// (b_B_fsig>2.)&&(b_tau_fsig>3.)&&b_tau_vprob>0.1&&(b_tau_alpha>0.)&&(b_B_r<1.)&&(b_B_mu_alpha>1.)&&(b_tau_rhomass_max>0.5)&&(b_tau_m<1.7)&&(b_B_m>3.)&&(b_B_m<5.28)
 		}
 		else 
 		{

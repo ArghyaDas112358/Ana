4b9f48c6955c345b21ec07b16400a70b842e19f4
Marc Huwiler <marc.huwiler@cern.ch>
2025-04-15 13:55:42 +0200

    Defining separate set of samples for fit

--- anaConfig.py
+++ anaConfig.py
@@ -21,7 +21,9 @@ dataWS = "dataDWS"
 
 Sig = "Sig"
 
-samples = [Sig, data, "B0toDstarDs", "B0toDstarDsstar", "B0toDstarD", "ButoDstarDK", "B0toDstarD0K", "B0toDstar3pi", "B0toDstar3pipi0", "B0toDstar5pi", "B0toDstarDs1", "B0toDstarDs0star", "B0toDstarD0Kstar", "ButoDstarDK", "BstoDD", "B0toDstara1"] #, dataWS #"SigPart", "dataD2WS", "dataD2TauWS", , "B0toDstar5pi" ["Sig", "dataD1", "B0toDstarDs", "B0toDstarDsstar", "B0toDstarD", "ButoDstarDK", "B0toDstarD0K", "BkgDstara1", "B0toDstar3pi", "dataD2WS"]
+samples = [Sig, data, "B0toDstarDs", "B0toDstarDsstar", "B0toDstarD", "ButoDstarDK", "B0toDstarD0K", "B0toDstar3pi"] ##[Sig, data, "B0toDstarDs", "B0toDstarDsstar", "B0toDstarD", "ButoDstarDK", "B0toDstarD0K", "B0toDstar3pi", "B0toDstar3pipi0", "B0toDstar5pi", "B0toDstarDs1", "B0toDstarDs0star", "B0toDstarD0Kstar", "BstoDD", "B0toDstara1"] #, dataWS #"SigPart", "dataD2WS", "dataD2TauWS", , "B0toDstar5pi" ["Sig", "dataD1", "B0toDstarDs", "B0toDstarDsstar", "B0toDstarD", "ButoDstarDK", "B0toDstarD0K", "BkgDstara1", "B0toDstar3pi", "dataD2WS"]
+
+fitsamples = [Sig, "data_obs", "B0toDstarDs", "B0toDstarDsstar", "B0toDstarD", "ButoDstarDK", "ABCD"] #"B0toDstara1"
 
 regions = ["SR", "CR", "SB"]
 
@@ -67,8 +69,10 @@ def Denominator():
 	Sig = "B0toDstarrho0pi"
 	global samples
 	global data
+	global fitsamples
 	data = "dataD"
 	samples = [data, Sig, "B0toDstar3pi", "B0toDstara1", "B0toDstar5pi", "B0toDstar3pipi0", "B0toDstarDs", "B0toDstarDsstar", "B0toDstarKpipi", "B0toDstarKpipi", "B0toDstarKKstar", "ButoDstarpipipipi0", "ButoDstarpipipi"]
+	fitsamples = [Sig, "data_obs", "B0toDstar3pi", "B0toDstara1", "B0toDstar5pi", "B0toDstar3pipi0", "B0toDstarDs", "B0toDstarDsstar", "B0toDstarKpipi", "B0toDstarKpipi", "B0toDstarKKstar", "ButoDstarpipipipi0", "ButoDstarpipipi"]
 	import anaPlotting 
 	anaPlotting.normalisebinwidth = True
 

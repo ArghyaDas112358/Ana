7d9db5e1c560fb1916d2537d3a8b9b7832611aca
Marc Huwiler marc.huwiler@cern.ch <mhuwiler@uaf-2.t2.ucsd.edu>
2025-11-10 02:13:28 -0800

    First pruning of repository

--- denomfit/runDenomFit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-python3 MakeFitModel.py
-combine -M MultiDimFit datacard.txt --saveWorkspace --algo singles --saveFitResult
-PostFitShapesFromWorkspace -w higgsCombineTest.MultiDimFit.mH120.root --output postFitPlots.root --fitresult multidimfitTest.root:fit_mdf --postfit
-python3 PlotDenomFit.py

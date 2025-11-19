6f11c66357cd0636581e776f4e6f755ca09929ea
mhuwiler <marc.huwiler@cern.ch>
2025-11-19 08:59:51 +0100

    Adding function for vector banches

--- HHbbtautauAnaElements.C
+++ HHbbtautauAnaElements.C
@@ -23,12 +23,33 @@ namespace Ana
 	}
 
 
+	ROOT::VecOps::RVec<TLorentzVector> computeP4Vec(ROOT::VecOps::RVec<double> pt, ROOT::VecOps::RVec<double> eta, ROOT::VecOps::RVec<double> phi, ROOT::VecOps::RVec<double> m)   
+	{
+		// Return a vector of P4 for collections with vector branches
+		ROOT::VecOps::RVec<TLorentzVector> P; 
+		int n = pt.size(); 
+		P.reserve(n); 
+
+		assert(eta.size() == n); 
+		assert(phi.size() == n); 
+		assert(m.size() == n); 
+
+		// Loop over the elements
+		for (unsigned int i=0; i<n; i++) 
+		{
+			P[i].SetPtEtaPhiM(pt[i], eta[i], phi[i], m[i]); 
+		}
+
+		return P; 
+	}
+
+
 	ROOT::RDF::RNode* GetP4(ROOT::RDF::RNode *frame, TString prefix, TString name = "") 
 	{
 		//TLorentzVector P4; 
 		std::string pfx = prefix.Data(); 
 		//ROOT::RDF::RNode *extended 
-		*frame = frame->Define(pfx+"_P4", computeP4, {pfx+"_pt", pfx+"_eta", pfx+"_phi", pfx+"_mass"});
+		*frame = frame->Define(pfx+"_P4", computeP4Vec, {pfx+"_pt", pfx+"_eta", pfx+"_phi", pfx+"_mass"});
 		return frame; 
 	}
 

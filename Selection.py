b6971085f2a5608b3d7a8e08627b76ae68113bbb
mhuwiler <marc.huwiler@cern.ch>
2025-12-09 23:02:49 +0100

    Adding function to find closest reco candidate

--- Selection.py
+++ Selection.py
@@ -101,6 +101,8 @@ if __name__ == "__main__":
 
 	sig = sig.Define("dR_mu_gen", "Ana::deltaR(GenDecay.mu, \"{1}\", {0}_pt, {0}_eta, {0}_phi, {0}_mass, {1}_pt, {1}_eta, {1}_phi, {1}_mass, {1}_pdgId)".format("GenPart", "Muon"))
 
+	sig = sig.Define("closest_mu_gen", "Ana::closestMatch(GenDecay.mu, \"{1}\", {0}_pt, {0}_eta, {0}_phi, {0}_mass, {1}_pt, {1}_eta, {1}_phi, {1}_mass, {1}_pdgId)".format("GenPart", "Muon"))
+
 	sig = Ana.GetGenParticles(sig, "Electron")
 
 	blacklist = ["Muon_P4", "GenPart_Particle", "GenMuon"] # TODO: add autoblacklist

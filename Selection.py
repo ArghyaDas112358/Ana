410a30511740cad3185b8216917cac5bc4db74e7
mhuwiler <marc.huwiler@cern.ch>
2025-12-09 12:52:31 +0100

    Adding fuoll gen muon information

--- Selection.py
+++ Selection.py
@@ -95,7 +95,9 @@ if __name__ == "__main__":
 
 	sig = sig.Define("GenPart_Particle", "Ana::computeP4Vec({0}_pt, {0}_eta, {0}_phi, {0}_mass, {0}_pdgId)".format("GenPart"))
 
-	sig = sig.Define("genType", "Ana::DecayGenMatching({0}_pdgId, {0}_genPartIdxMother, {0}_statusFlags)".format("GenPart"))
+	sig = sig.Define("GenDecay", "Ana::DecayGenMatching({0}_pdgId, {0}_genPartIdxMother, {0}_statusFlags)".format("GenPart"))
+
+	sig = sig.Define("TheGenMuon_pt", "GenPart_pt[GenDecay.decayType]").Define("TheGenMuon_eta", "GenPart_eta[GenDecay.decayType]").Define("TheGenMuon_phi", "GenPart_phi[GenDecay.decayType]")
 
 	sig = Ana.GetGenParticles(sig, "Electron")
 
